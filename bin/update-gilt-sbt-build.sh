bd8bdfa35772d1e7c238a3deebc1d4aefecd61dc
Myyk Seok <mseok@gilt.com>
2015-04-13 02:49:05 -0700

    Script to update gilt-sbt-build versions.

---
+++ bin/update-gilt-sbt-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VERSION="6.4.0"
+
+while read -r LINE || [[ -n $LINE ]]; do
+  printf "%s\n" "${LINE}"
+  cd "/web/${LINE}"
+  git checkout master
+  git pull
+  git checkout -b "upgrade-to-g-s-b-$VERSION"
+  sed -i "" -E 's/addSbtPlugin\("com.giltgroupe"[[:space:]]+%[[:space:]]+"gilt-sbt-build"[[:space:]]+%[[:space:]]+"[[:digit:]]+.[[:digit:]]+.[[:digit:]]+"\)/addSbtPlugin("com.giltgroupe" % "gilt-sbt-build" % "'"$VERSION"'")/g' project/build.sbt 
+  git commit -a -m "NOJIRA-0: Upgrade gilt-sbt-build tp $VERSION"
+  git push origin HEAD:refs/for/master
+done < projects.txt
