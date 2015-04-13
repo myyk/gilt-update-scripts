96015cce6d0c2770f421014a4810c889162cec40
Karl Norling <knorling@gilt.com>
2015-04-13 12:09:18 -0400

    Added branch.

--- bin/update-gilt-sbt-build.sh
+++ bin/update-gilt-sbt-build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 VERSION="6.4.0"
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+printf "On branch: %s\n" "$BRANCH"
 
 while read -r LINE || [[ -n $LINE ]]; do
   printf "%s\n" "${LINE}"
@@ -10,5 +13,5 @@ while read -r LINE || [[ -n $LINE ]]; do
   git checkout -b "upgrade-to-g-s-b-$VERSION"
   sed -i "" -E 's/addSbtPlugin\("com.giltgroupe"[[:space:]]+%[[:space:]]+"gilt-sbt-build"[[:space:]]+%[[:space:]]+"[[:digit:]]+.[[:digit:]]+.[[:digit:]]+"\)/addSbtPlugin("com.giltgroupe" % "gilt-sbt-build" % "'"$VERSION"'")/g' project/build.sbt 
   git commit -a -m "NOJIRA-0: Upgrade gilt-sbt-build tp $VERSION"
-  git push origin HEAD:refs/for/master
+  git push origin HEAD:refs/for/$BRANCH
 done < projects.txt
