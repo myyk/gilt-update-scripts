7360eb7748d22a3317414f1405d018bda67170ab
Myyk Seok <mseok@gilt.com>
2015-04-22 17:04:52 -0700

    Fixed update-sbt.sh.

--- bin/update-sbt.sh
+++ bin/update-sbt.sh
@@ -8,9 +8,7 @@ while read -r LINE || [[ -n $LINE ]]; do
   git checkout master
   git pull
   git checkout -b "upgrade-sbt-to-$VERSION"
-  sbt.version=0.13.8
-
   sed -i "" -E 's/sbt.version[[:space:]]*=[[:space:]]*[[:digit:]]+.[[:digit:]]+.[[:digit:]]+/sbt.version='"$VERSION"'/g' project/*.properties 
-  git commit -m "NOJIRA-0: Upgrade sbt to $VERSION" project/build.sbt
+  git commit -m "NOJIRA-0: Upgrade sbt to $VERSION" project/*.properties
   git push origin HEAD:refs/for/master
 done < projects.txt
